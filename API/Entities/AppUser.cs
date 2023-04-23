64f5915238e8523dcee862301449cc3f75bc29c3
Leonard Okiti <zin.leonardo@gmail.com>
2023-04-23 10:07:02 -0400

    end of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+// These are not used and can be removed
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
